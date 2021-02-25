b78b0721e2b8b07879193981af7f39953022d072
MATTHEW SIMMS <M.PARKER.SIMMS@GMAIL.COM>
2021-02-25 10:53:02 -0600

    challenge 7 completed

--- code-challenges/challenges-08.test.js
+++ code-challenges/challenges-08.test.js
@@ -137,6 +137,14 @@ const snorlaxData = {
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
   // Solution code here...
+  const newArr = arr.filter(value => {
+    if (value.baseStat > minBaseStat){
+      return true;
+    }else {
+      return false;
+    }
+  });
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -298,7 +306,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array containing the stats that are greater than the input', () => {
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75)).toStrictEqual([{ stat: { url: 'https://pokeapi.co/api/v2/stat/5/', name: 'special-defense' }, effort: 2, baseStat: 110 }]);
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75).length).toStrictEqual(1);
